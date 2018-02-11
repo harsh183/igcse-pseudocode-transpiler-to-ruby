be9600c1f190927a3a7972fbd3ba449efd4fc538
Harsh Deep <hrshdeep51@gmail.com>
2018-02-11 17:11:35 +0530

    Add better array handling

--- main.rb
+++ main.rb
@@ -2,191 +2,208 @@ require 'pry'
 
 # TODO: Make sure the code conversions don't affect things inside strings (maybe write your own version of gsub that ignores comments and strings)
 # TODO: Escape variable names that contain part of a command
-def convert(code)
-  output_text = ''
-  output_lines = []
-  code.lines.each do |line|
-    # Handle comments
-    line = handle_comments(line)
+class Converter
+  attr_accessor :arrays
 
-    line = handle_type_conversions(line)
+  def convert(code)
+    output_text = ''
+    output_lines = []
+    code.lines.each do |line|
+      # Handle comments
+      line = handle_comments(line)
+      line = handle_type_conversions(line)
 
-    # Convert all the endings of blocks properly
-    block_endings = ['endif', 'endfunction', 'endwhile', 'endprocedure', 'endswitch']
-    block_endings.each do |ending|
-      line.gsub!(ending, 'end')
+      # Convert all the endings of blocks properly
+      block_endings = ['endif', 'endfunction', 'endwhile', 'endprocedure', 'endswitch']
+      block_endings.each do |ending|
+        line.gsub!(ending, 'end')
+      end
+
+      # Get rid of then
+      line.gsub!('then', '')
+
+      # Convert elseif to elsif
+      line.gsub!('elseif', 'elsif')
+
+
+      line = handle_switch_case(line)
+
+      # Convert logical operations to ruby equivalent
+      logical_operations = { 'MOD' => '%',
+                             'DIV' => '/',
+                             '^'   => '**' }
+      logical_operations.each do |operation, ruby_operation|
+        line.gsub!(operation.to_s, ruby_operation)
+      end
+
+      line = handle_array_init(line)
+      line = handle_multidimensional_array_assign(line)
+
+      # Handle functions
+      line.gsub!('function', 'def')
+
+      # Handle procedures
+      line.gsub!('procedure', 'def')
+
+
+      line = handle_for_loops(line)
+
+      # Handle do..until loops
+      # Note: Since I don't think there are any exit controlled loops in ruby
+      #       I'm making one by hand using an infinite loop and break statements
+      line.gsub!('do', 'while true')
+      line = handle_do_until_loop_end(line)
+
+      line = handle_substring(line)
+
+      output_lines.push line
     end
 
-    # Get rid of then
-    line.gsub!('then', '')
-
-    # Convert elseif to elsif
-    line.gsub!('elseif', 'elsif')
-
-    # Handle switch-case
-    line = handle_switch_case(line)
-
-    # Convert logical operations to ruby equivalent
-    logical_operations = { 'MOD' => '%',
-                           'DIV' => '/',
-                           '^'   => '**' }
-    logical_operations.each do |operation, ruby_operation|
-      line.gsub!(operation.to_s, ruby_operation)
-    end
-
-    # Handle arrays
-    # line = handle_array_init(line)
-
-    # Handle functions
-    line.gsub!('function', 'def')
-
-    # Handle procedures
-    line.gsub!('procedure', 'def')
-
-    # Handle for loops
-    line = handle_for_loops(line)
-
-    # Handle do..until loops
-    # Note: Since I don't think there are any exit controlled loops in ruby
-    #       I'm making one by hand using an infinite loop and break statements
-    line.gsub!('do', 'while true')
-    line = handle_do_until_loop_end(line)
-
-    # Handle substring
-    line = handle_substring(line)
-
-    output_lines.push line
+    output_text << output_lines.join()
   end
 
-  output_text << output_lines.join("\n")
-end
+  def handle_multidimensional_array_assign(line)
+    if(line.include? '=')
+      address_in_psudocode = '[' + line.split('=').first.split('[').last # EX: [5, 10, 2]
+      address_in_psudocode = address_in_psudocode.delete(' ')
+      address_in_ruby = address_in_psudocode.gsub(',', '][')
+      line = line.gsub(address_in_psudocode, address_in_ruby)
+    end
+    return line;
+  end
 
 # Type conversions (from psudocode to ruby)
-def handle_type_conversions(line)
-  type_conversions = { str: 'String',
-                       int: 'Integer',
-                       float: 'Float' }
-  type_conversions.each do |type, ruby_code|
-    # TODO: Better way to skip print (since it has int in it's name)
-    unless line.index('print').nil?
-      next
+  def handle_type_conversions(line)
+    type_conversions = { str: 'String',
+                         int: 'Integer',
+                         float: 'Float' }
+    type_conversions.each do |type, ruby_code|
+      # TODO: Better way to skip print (since it has int in it's name)
+      unless line.index('print').nil?
+        next
+      end
+
+      line = line.gsub(type.to_s, ruby_code)
     end
 
-    line = line.gsub(type.to_s, ruby_code)
+    return line
   end
-
-  return line
-end
 
 # TODO: Escape strings
-def handle_comments(line)
-  if line.start_with?('//')
-    line = ''
-  elsif line.include? '//'
-    line = line[0..line.index('//') - 1]
+  def handle_comments(line)
+    if line.start_with?('//')
+      line = ''
+    elsif line.include? '//'
+      line = line[0..line.index('//') - 1]
+    end
+    return line
   end
-  return line
-end
 
 # TODO: Make this more generalized (i.e this only works when the array is declared on it's own line)
-def handle_array_init(line)
-  line = line.gsub('Array', 'array')
-  if line.include?('array')
-    variable_name = line.split('array').last
-    variable_name = variable_name.delete("\n")
-    dimensions = '[' + variable_name.split('[').last
-    binding.pry
+  def handle_array_init(line)
+    line = line.gsub('Array', 'array')
+    if line.include?('array')
+      array_identifer = line.split('array').last
+      array_identifer = array_identifer.delete("\n")
+      #varible_name = array_identifer.split('[').first
+      dimensions = '[' + array_identifer.split('[').last
+      function_args = dimensions.gsub('][', ', ')
+      #binding.pry
+      line.slice!('array')
+      line = line.gsub(dimensions, " = initArray(#{function_args})")
+    end
+    return line
   end
-  return line
-end
 
 # Note: Since psudocode uses case to indicate each branch and ruby
 #       uses case to indicate the block, we should parse case first
 #       and then the block header
-def handle_switch_case(line)
-  line = handle_case_header(line)
-  line = handle_switch_header(line)
-  line = handle_switch_default(line)
-  return line
-end
-
-def handle_case_header(line)
-  line = line.gsub('case', 'when')
-  line = line.delete(':')
-  return line
-end
-
-def handle_switch_header(line)
-  line = line.gsub('switch', 'case')
-  line = line.delete(':')
-  return line
-end
-
-def handle_switch_default(line)
-  line = line.gsub('default', 'else')
-  line = line.delete(':')
-  return line
-end
-
-def handle_do_until_loop_end(line)
-  if line.include?('until')
-    condition = line.split('until').last
-
-    line = "    break if #{condition}\nend"
+  def handle_switch_case(line)
+    line = handle_case_header(line)
+    line = handle_switch_header(line)
+    line = handle_switch_default(line)
+    return line
   end
 
-  return line
-end
-
-def handle_for_loops(line)
-  line = handle_for_loop_header(line)
-  line = handle_for_loop_end(line)
-  return line
-end
-
-def handle_for_loop_header(line)
-  unless line.index('for').nil?
-    range_text = line.split('=')[1]
-    range_code_for_ruby = range_text.delete(' ').gsub('to', '..')
-    line.gsub!('=', ' in ')
-    line.gsub!(range_text, range_code_for_ruby)
+  def handle_case_header(line)
+    line = line.gsub('case', 'when')
+    line = line.delete(':')
+    return line
   end
 
-  return line
-end
+  def handle_switch_header(line)
+    line = line.gsub('switch', 'case')
+    line = line.delete(':')
+    return line
+  end
 
-def handle_for_loop_end(line)
-  line = 'end' unless line.index('next').nil?
-  return line
-end
+  def handle_switch_default(line)
+    line = line.gsub('default', 'else')
+    line = line.delete(':')
+    return line
+  end
+
+  def handle_do_until_loop_end(line)
+    if line.include?('until')
+      condition = line.split('until').last
+
+      line = "    break if #{condition}\nend"
+    end
+
+    return line
+  end
+
+  def handle_for_loops(line)
+    line = handle_for_loop_header(line)
+    line = handle_for_loop_end(line)
+    return line
+  end
+
+  def handle_for_loop_header(line)
+    unless line.index('for').nil?
+      range_text = line.split('=')[1]
+      range_code_for_ruby = range_text.delete(' ').gsub('to', '..')
+      line.gsub!('=', ' in ')
+      line.gsub!(range_text, range_code_for_ruby)
+    end
+
+    return line
+  end
+
+  def handle_for_loop_end(line)
+    line = 'end' unless line.index('next').nil?
+    return line
+  end
 
 # Note: IGCSE defines subString(startingPosition, numberOfCharacters), this is equivalent to ruby slice
-def handle_substring(line)
-  line = line.gsub('substring', 'slice')
-  return line
-end
-
-def get_matching_bracket(string, open_bracket_index)
-  index = open_bracket_index
-  depth = 0 # Depth is how deep within a nested bracket is
-
-  chars = string[index..string.length]
-
-  chars.each_char do |char|
-    if char == '('
-      depth += 1
-    elsif char == ')'
-      depth -= 1
-    end
-
-    if depth == 0
-      break
-    end
-
-    index += 1
+  def handle_substring(line)
+    line = line.gsub('substring', 'slice')
+    return line
   end
 
-  return index
+  def get_matching_bracket(string, open_bracket_index)
+    index = open_bracket_index
+    depth = 0 # Depth is how deep within a nested bracket is
+
+    chars = string[index..string.length]
+
+    chars.each_char do |char|
+      if char == '('
+        depth += 1
+      elsif char == ')'
+        depth -= 1
+      end
+
+      if depth == 0
+        break
+      end
+
+      index += 1
+    end
+
+    return index
+  end
+
 end
 
 output_text = ''
@@ -206,6 +223,15 @@ output_text << 'end'+ "\n"
 output_text << "\n"
 
 # Create an array init function
+output_text << "def initArray(dimensions=[10], default_element=nil) \n"
+output_text << "  size = dimensions.first \n"
+output_text << "  if dimensions.length > 1 \n"
+output_text << "    dimensions.shift \n"
+output_text << "    return Array.new(size, initArray(dimensions)) \n"
+output_text << "  else \n"
+output_text << "    return Array.new(size, default_element) \n"
+output_text << "  end \n"
+output_text << " end \n"
 
 File.open('input.txt', 'r') do |f|
   code = f.read
--- output.rb
+++ output.rb
@@ -8,182 +8,103 @@ def print(string="")
   puts(string)
 end
 
+def initArray(dimensions=[10], default_element=nil) 
+  size = dimensions.first 
+  if dimensions.length > 1 
+    dimensions.shift 
+    return Array.new(size, initArray(dimensions)) 
+  else 
+    return Array.new(size, default_element) 
+  end 
+ end 
 print("Hello World")
 
-
-
 x=3
-
 name="Bob"
 
-
-
 y=String(3*27)
-
 Integer("3")
-
 Float("3.14")
 
-
-
 print(x)
-
 print(y)
-
 print(name)
 
-
-
 def triple(number)
-
     return number*3
-
 end
 
-
-
 y=triple(7)
-
 print(y)
 
-
-
-
-array names[5]
-
-names[0]=”Ahmad”
-
-names[1]=”Ben”
-
-names[2]=”Catherine”
-
-names[3]=”Dana”
-
-names[4]=”Elijah”
-
+ names = initArray([5])
+names[0]="Ahmad"
+names[1]="Ben"
+names[2]="Catherine"
+names[3]="Dan"
+names[4]="Elijah"
 print(names[3])
 
-
+ board = initArray([8,8])
+board[0][0]="rook"
+print(board[0,0])
 
 entry=input("Please enter your name")
 
-
-
 if entry=="Harsh" 
-
-    print("You selected Harsh") 
-elsif entry=="B" 
-
+    print("You selected Harsh") elsif entry=="B" 
     print("You selected B")
-
 else
-
     print("Unrecognised selection")
-
 end
-
-
-
 
 def greeting(name)
-
     print("anyway, hello "+name)
-
 end
-
-
 
 greeting(entry)
 
-
-
 someText="Computer Science"
-
 print(someText.length)
-
 print(someText.slice(3,3))
 
-
-
-
 for i in 0..7
-
     print(i)
-
 end
-
-
 while true
-
     answer=input("What is the answer to life, the universe and everything?")
-
     break if  answer=="42"
 
 end
-
-
 while (x % 10 <= 5)
-
     print(x)
-
     x = x + 1
-
 end
-
-
 
 print()
 
-
-
 print("Counting from given input to 0")
 
-
-
 def countDownFrom(number)
-
     while(number > 0)
-
         print(number)
-
         number = number - 1
-
     end
-
 end
-
-
-
-
-
-
 
 
 
 countDownAmount = 20
-
 countDownFrom(countDownAmount)
-
-
 
 print()
 
-
-
 something = input("Enter anything")
-
 case something
-
     when "A"
-
          print("You picked A")
-
     when "42"
-
          print("#Douglas Adams")
-
     else
-
          print("You are a slave to the system")
-
 end
